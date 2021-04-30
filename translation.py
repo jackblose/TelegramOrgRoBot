33560823bbe41c92ac537c6999ea48b586493728
yunuszenduserbot <79845960+yunuszenduserbot@users.noreply.github.com>
2021-04-30 23:20:40 +0000

    Transalation.py

--- translation.py
+++ translation.py
@@ -1,20 +1,17 @@
 class Translation(object):
-    START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+    START_TEXT = """**Hallo Kamu!**
+_**Terima Kasih sudah menggunakan bot saya**_
+Silahkan kirimkan nomor HP kamu dengan awalan +62 untuk mendapatkan kode dari telegram
 
 /start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
+    AFTER_RECVD_CODE_TEXT = """**Oke !**
+_Sekarang tolong kirimkan kode yg baru anda peroleh dari pihak telegram!_
 
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
+**kode ini hanya digunakan dengan tujuan mendapatkan _API ID dan HASH_ dari my.telegram.org**
 
 /start at any stage to re-enter your details"""
-    BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
-    CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    BEFORE_SUCC_LOGIN = "Oke Kode anda sudah diterima ..."
+    ERRED_PAGE = "ada sesuatu yang salah. gagal mendapatkan id aplikasi. \n\n@SpEcHlDe"
+    CANCELLED_MESG = "Selamat tinggal! Tolong re /start bot ini untuk memulai kembali"
+    IN_VALID_CODE_PVDED = "maaf, tampaknya masukan tersebut bukan kode Login Web Telegram yang valid "
+    IN_VALID_PHNO_PVDED = "maaf, masukan tersebut sepertinya bukan nomor telepon yang valid"
