285011a40da8bc85a14898b1918309cb59dc72c7
yunuszenduserbot <79845960+yunuszenduserbot@users.noreply.github.com>
2021-05-01 00:21:29 +0000

    Translation.py

--- translation.py
+++ translation.py
@@ -1,11 +1,11 @@
 class Translation(object):
-    START_TEXT = """Hallo! Silahkan kirimkan nomor HP kamu dengan awalan +62 untuk mendapatkan kode dari telegram"""
-    AFTER_RECVD_CODE_TEXT = """Sekarang tolong kirimkan kode yg baru anda peroleh dari pihak telegram!
-kode ini hanya digunakan dengan tujuan mendapatkan API ID dan HASH dari my.telegram.org.
+    START_TEXT = """<b>Hallo! Silahkan kirimkan nomor HP kamu dengan awalan +62 untuk mendapatkan kode dari telegram</b>"""
+    AFTER_RECVD_CODE_TEXT = """<b>Sekarang tolong kirimkan kode yg baru anda peroleh dari pihak telegram</b>!
+<i>kode ini hanya digunakan dengan tujuan mendapatkan API ID dan HASH dari my.telegram.org.</i>
 
-/start untuk memulai kembali"""
-    BEFORE_SUCC_LOGIN = "Oke Kode anda sudah diterima ..."
-    ERRED_PAGE = "ada sesuatu yang salah. gagal mendapatkan Api ID dan HASH kamu. \n\n@ZendYNS"
-    CANCELLED_MESG = "Selamat tinggal! Tolong re /start bot ini untuk memulai kembali"
-    IN_VALID_CODE_PVDED = "maaf, tampaknya masukan tersebut bukan kode Login Web Telegram yang valid "
-    IN_VALID_PHNO_PVDED = "maaf, masukan tersebut sepertinya bukan nomor telepon yang valid"
+<b>/start untuk memulai kembali</b>"""
+    BEFORE_SUCC_LOGIN = "<b>Oke Kode anda sudah diterima, bentar yah😊...</b>"
+    ERRED_PAGE = "<b>ada sesuatu yang salah. gagal mendapatkan Api ID dan HASH kamu.</b> \n\n Contact Me @ZendYNS"
+    CANCELLED_MESG = "<i>Selamat tinggal!</i> </b>Tolong re /start bot ini untuk memulai kembali</b>"
+    IN_VALID_CODE_PVDED = "<b>maaf, tampaknya masukan tersebut bukan kode Login Web Telegram yang valid </b>"
+    IN_VALID_PHNO_PVDED = "<b>maaf, masukan tersebut sepertinya bukan nomor telepon yang valid</b>"
