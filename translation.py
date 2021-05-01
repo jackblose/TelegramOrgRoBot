0293804f49decebb1e92c66904cc9f4e69b36e02
yunuszenduserbot <79845960+yunuszenduserbot@users.noreply.github.com>
2021-05-01 00:25:26 +0000

    Translation.py

--- translation.py
+++ translation.py
@@ -4,8 +4,8 @@ class Translation(object):
 <i>kode ini hanya digunakan dengan tujuan mendapatkan API ID dan HASH dari my.telegram.org.</i>
 
 <b>/start untuk memulai kembali</b>"""
-    BEFORE_SUCC_LOGIN = "<b>Oke Kode anda sudah diterima, bentar yah😊...</b>"
-    ERRED_PAGE = "<b>ada sesuatu yang salah. gagal mendapatkan Api ID dan HASH kamu.</b> \n\n Contact Me @ZendYNS"
-    CANCELLED_MESG = "<i>Selamat tinggal!</i> </b>Tolong re /start bot ini untuk memulai kembali</b>"
-    IN_VALID_CODE_PVDED = "<b>maaf, tampaknya masukan tersebut bukan kode Login Web Telegram yang valid </b>"
-    IN_VALID_PHNO_PVDED = "<b>maaf, masukan tersebut sepertinya bukan nomor telepon yang valid</b>"
+    BEFORE_SUCC_LOGIN = "Oke Kode anda sudah diterima, bentar yah😊..."
+    ERRED_PAGE = "ada sesuatu yang salah. gagal mendapatkan Api ID dan HASH kamu. \n\n Contact Me @ZendYNS"
+    CANCELLED_MESG = "Selamat tinggal! Tolong re /start bot ini untuk memulai kembali"
+    IN_VALID_CODE_PVDED = "maaf, tampaknya masukan tersebut bukan kode Login Web Telegram yang valid "
+    IN_VALID_PHNO_PVDED = "maaf, masukan tersebut sepertinya bukan nomor telepon yang valid"
